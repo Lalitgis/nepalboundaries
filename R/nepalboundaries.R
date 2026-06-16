d48bf444f2e7e0df5c7fdcaec3a3d5060b760dd4
Lalit BC <89679878+Lalitgis@users.noreply.github.com>
2026-06-16 14:05:44 -0400

    Update national park function to include protected areas

--- R/nepalboundaries.R
+++ R/nepalboundaries.R
@@ -355,10 +355,12 @@ nb_get_multiple <- function(
 nb_summary <- function(level = "district") {
 
   valid_levels <- c(
+    "country"
     "province",
     "district",
     "municipality",
     "ward"
+    "nationalpark"
   )
 
   level <- match.arg(level, valid_levels)
@@ -471,9 +473,9 @@ nb_province_headquarters <- function(province = NULL) {
 }
 #' Get Nepal National Park Boundaries
 #'
-#' Retrieves national park boundaries of Nepal.
+#' Retrieves national park and protected area boundaries of Nepal.
 #'
-#' @param park Character vector. Optional national park name(s).
+#' @param park Character vector. Optional national park / protected area name(s).
 #'   If NULL, all national parks are returned.
 #'
 #' @return An sf object containing national park boundaries.
@@ -487,26 +489,38 @@ nb_province_headquarters <- function(province = NULL) {
 #' @export
 nb_nationalpark <- function(park = NULL) {
 
-  park_data <- load_nb_data("nationalpark")
+  data <- readRDS(
+    system.file(
+      "data",
+      "nationalpark.rds",
+      package = "nepalboundaries"
+    )
+  )
+
+  # standardize column name
+  if (!"park_name" %in% names(data) && "NAME" %in% names(data)) {
+    names(data)[names(data) == "NAME"] <- "park_name"
+  }
 
   validate_columns(
-    park_data,
-    c("NAME")  # change if your column name differs
+    data,
+    c("park_name")
   )
 
   if (!is.null(park)) {
 
-    park_data <- park_data |>
+    data <- data |>
       dplyr::filter(
-        tolower(park_name) %in% tolower(park)
+        tolower(park_name) %in%
+          tolower(park)
       )
 
-    if (nrow(park_data) == 0) {
+    if (nrow(data) == 0) {
       warning("No national parks found matching criteria.")
     }
   }
 
-  park_data
+  data
 }
 
 #' Internal Function to Load Nepal Boundary Data
